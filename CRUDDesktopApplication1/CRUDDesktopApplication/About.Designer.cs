3623cfb80b2d022325de4f6c2a991ee8801305a0
Md. Pias Uddin <52478932+Piasuddin@users.noreply.github.com>
2019-07-31 22:52:30 +0600

    Add files via upload

---
+++ CRUDDesktopApplication1/CRUDDesktopApplication/About.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace CRUDDesktopApplication
+{
+    partial class About
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Palatino Linotype", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(94, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(141, 36);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Created by";
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.label1);
+            this.panel4.Controls.Add(this.label3);
+            this.panel4.Controls.Add(this.label4);
+            this.panel4.Controls.Add(this.label2);
+            this.panel4.Font = new System.Drawing.Font("Palatino Linotype", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel4.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.panel4.Location = new System.Drawing.Point(254, 80);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(328, 190);
+            this.panel4.TabIndex = 12;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Palatino Linotype", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Location = new System.Drawing.Point(64, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(201, 36);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Md. Pias Uddin";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Palatino Linotype", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label4.Location = new System.Drawing.Point(85, 133);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(158, 36);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "ID: 1248677";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(320, 36);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "This is simple desktop app";
+            // 
+            // About
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(45)))), ((int)(((byte)(60)))));
+            this.Controls.Add(this.panel4);
+            this.Name = "About";
+            this.Size = new System.Drawing.Size(850, 405);
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ CRUDDesktopApplication1/CRUDDesktopApplication/Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace CRUDDesktopApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnAbout = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.slidePanel = new System.Windows.Forms.Panel();
+            this.btnViewInfo = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonCircular1 = new CRUDDesktopApplication.ButtonCircular();
+            this.userControl11 = new CRUDDesktopApplication.UserControl1();
+            this.userControlCRUDOperation2 = new CRUDDesktopApplication.userControlCRUDOperation();
+            this.about1 = new CRUDDesktopApplication.About();
+            this.infoUserControl1 = new CRUDDesktopApplication.InfoUserControl();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(49)))), ((int)(((byte)(82)))));
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.buttonCircular1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1039, 41);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(106)))), ((int)(((byte)(27)))));
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Controls.Add(this.pictureBox1);
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(183, 41);
+            this.panel3.TabIndex = 8;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(32)))), ((int)(((byte)(41)))));
+            this.panel2.Controls.Add(this.btnAbout);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.slidePanel);
+            this.panel2.Controls.Add(this.btnViewInfo);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Location = new System.Drawing.Point(1, 2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(181, 451);
+            this.panel2.TabIndex = 1;
+            // 
+            // btnAbout
+            // 
+            this.btnAbout.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAbout.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(48)))), ((int)(((byte)(89)))));
+            this.btnAbout.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(48)))), ((int)(((byte)(89)))));
+            this.btnAbout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAbout.Font = new System.Drawing.Font("Segoe UI Emoji", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAbout.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.btnAbout.Image = ((System.Drawing.Image)(resources.GetObject("btnAbout.Image")));
+            this.btnAbout.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnAbout.Location = new System.Drawing.Point(10, 182);
+            this.btnAbout.Name = "btnAbout";
+            this.btnAbout.Size = new System.Drawing.Size(171, 46);
+            this.btnAbout.TabIndex = 8;
+            this.btnAbout.Text = "About";
+            this.btnAbout.UseVisualStyleBackColor = true;
+            this.btnAbout.Click += new System.EventHandler(this.BtnAbout_Click);
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(48)))), ((int)(((byte)(89)))));
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(48)))), ((int)(((byte)(89)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Segoe UI Emoji", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.Location = new System.Drawing.Point(10, 87);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(171, 46);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "CRUD Operation";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // slidePanel
+            // 
+            this.slidePanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(49)))), ((int)(((byte)(82)))));
+            this.slidePanel.Location = new System.Drawing.Point(1, 40);
+            this.slidePanel.Name = "slidePanel";
+            this.slidePanel.Size = new System.Drawing.Size(10, 46);
+            this.slidePanel.TabIndex = 7;
+            // 
+            // btnViewInfo
+            // 
+            this.btnViewInfo.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnViewInfo.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(48)))), ((int)(((byte)(89)))));
+            this.btnViewInfo.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(48)))), ((int)(((byte)(89)))));
+            this.btnViewInfo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnViewInfo.Font = new System.Drawing.Font("Segoe UI Emoji", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnViewInfo.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.btnViewInfo.Image = ((System.Drawing.Image)(resources.GetObject("btnViewInfo.Image")));
+            this.btnViewInfo.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnViewInfo.Location = new System.Drawing.Point(11, 134);
+            this.btnViewInfo.Name = "btnViewInfo";
+            this.btnViewInfo.Size = new System.Drawing.Size(171, 46);
+            this.btnViewInfo.TabIndex = 5;
+            this.btnViewInfo.Text = "View Info";
+            this.btnViewInfo.UseVisualStyleBackColor = true;
+            this.btnViewInfo.Click += new System.EventHandler(this.BtnViewInfo_Click);
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(48)))), ((int)(((byte)(89)))));
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(48)))), ((int)(((byte)(89)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI Emoji", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(11, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(171, 46);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Home";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(8, 7);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(29, 28);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Silver;
+            this.label1.Location = new System.Drawing.Point(37, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "IDB-BISEW";
+            // 
+            // buttonCircular1
+            // 
+            this.buttonCircular1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonCircular1.FlatAppearance.BorderSize = 0;
+            this.buttonCircular1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonCircular1.Image = ((System.Drawing.Image)(resources.GetObject("buttonCircular1.Image")));
+            this.buttonCircular1.Location = new System.Drawing.Point(992, 8);
+            this.buttonCircular1.Name = "buttonCircular1";
+            this.buttonCircular1.Size = new System.Drawing.Size(26, 24);
+            this.buttonCircular1.TabIndex = 2;
+            this.buttonCircular1.UseVisualStyleBackColor = false;
+            this.buttonCircular1.Click += new System.EventHandler(this.ButtonCircular1_Click);
+            // 
+            // userControl11
+            // 
+            this.userControl11.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(45)))), ((int)(((byte)(60)))));
+            this.userControl11.Location = new System.Drawing.Point(184, 43);
+            this.userControl11.Name = "userControl11";
+            this.userControl11.Size = new System.Drawing.Size(850, 410);
+            this.userControl11.TabIndex = 6;
+            // 
+            // userControlCRUDOperation2
+            // 
+            this.userControlCRUDOperation2.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.userControlCRUDOperation2.Location = new System.Drawing.Point(184, 42);
+            this.userControlCRUDOperation2.Name = "userControlCRUDOperation2";
+            this.userControlCRUDOperation2.Size = new System.Drawing.Size(852, 412);
+            this.userControlCRUDOperation2.TabIndex = 7;
+            // 
+            // about1
+            // 
+            this.about1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(45)))), ((int)(((byte)(60)))));
+            this.about1.Location = new System.Drawing.Point(184, 42);
+            this.about1.Name = "about1";
+            this.about1.Size = new System.Drawing.Size(850, 412);
+            this.about1.TabIndex = 12;
+            // 
+            // infoUserControl1
+            // 
+            this.infoUserControl1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(45)))), ((int)(((byte)(60)))));
+            this.infoUserControl1.Location = new System.Drawing.Point(184, 42);
+            this.infoUserControl1.Name = "infoUserControl1";
+            this.infoUserControl1.Size = new System.Drawing.Size(852, 412);
+            this.infoUserControl1.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(45)))), ((int)(((byte)(60)))));
+            this.ClientSize = new System.Drawing.Size(1039, 457);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.userControl11);
+            this.Controls.Add(this.userControlCRUDOperation2);
+            this.Controls.Add(this.about1);
+            this.Controls.Add(this.infoUserControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private ButtonCircular buttonCircular1;
+        private System.Windows.Forms.Button button1;
+        private userControlCRUDOperation userControlCRUDOperation1;
+        private UserControl1 userControl11;
+        private System.Windows.Forms.Panel slidePanel;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnAbout;
+        private System.Windows.Forms.Button btnViewInfo;
+        private System.Windows.Forms.Panel panel3;
+        private userControlCRUDOperation userControlCRUDOperation2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private About about1;
+        private InfoUserControl infoUserControl1;
+    }
+}
+
